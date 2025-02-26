7e622d98342fe0fa51db4e02ea5c082317ff6535
ui3o <43638725+ui3o@users.noreply.github.com>
2025-02-26 15:52:02 +0000

    mobile demo add

--- src/components/ui/TimeLine.tsx
+++ src/components/ui/TimeLine.tsx
@@ -72,22 +72,24 @@ export default class TimeLine extends React.Component<Props, State> {
 
     return (
       <>
-        <section className="relative min-h-screen flex flex-col justify-center bg-slate-50 overflow-hidden">
+        <section className=" in-h-screen flex flex-col justify-center bg-slate-50">
+          <div className="flex flex-col justify-center sticky top-0 z-10 pb-2">
+            <div className="w-full max-w-3xl mx-auto">
+              <div className="flex justify-between text-4xl mb-2 bg-white ">
+                {actualTimer % 2 === 0 &&
+                  <div className={`center min-w-9/12 select-none whitespace-nowrap rounded-lg py-2 px-3.5 align-baseline font-sans  font-bold uppercase leading-none text-white bg-red-500 mr-2`}>
+                    <div className="mt-px">{this.calculateTimer(actualTimer % 2 === 0, actualTimer)}</div>
+                  </div>
+                }
+                <div className={`center min-w-2/12 select-none whitespace-nowrap rounded-lg py-2 px-3.5 align-baseline font-sans  font-bold uppercase leading-none text-white ${actualTimer % 2 !== 0 ? "bg-green-700" : "bg-green-500"} w-full`}>
+                  <div className="mt-px">{this.calculateTimer(actualTimer % 2 !== 0, actualTimer % 2 !== 0 ? actualTimer : actualTimer + 1)}</div>
+                </div>
+              </div>
+            </div>
+          </div>
           <div className="w-full max-w-6xl mx-auto px-4 md:px-6 py-10">
             <div className="flex flex-col justify-center divide-y divide-slate-200">
               <div className="w-full max-w-3xl mx-auto">
-
-                <div className="flex justify-between text-4xl mb-2">
-                  {actualTimer % 2 === 0 &&
-                    <div className={`center min-w-9/12 select-none whitespace-nowrap rounded-lg py-2 px-3.5 align-baseline font-sans  font-bold uppercase leading-none text-white bg-red-500 mr-2`}>
-                      <div className="mt-px">{this.calculateTimer(actualTimer % 2 === 0, actualTimer)}</div>
-                    </div>
-                  }
-                  <div className={`center min-w-2/12 select-none whitespace-nowrap rounded-lg py-2 px-3.5 align-baseline font-sans  font-bold uppercase leading-none text-white ${actualTimer % 2 !== 0 ? "bg-green-700" : "bg-green-500"} w-full`}>
-                    <div className="mt-px">{this.calculateTimer(actualTimer % 2 !== 0, actualTimer % 2 !== 0 ? actualTimer : actualTimer + 1)}</div>
-                  </div>
-                </div>
-
                 <div className="space-y-8 relative before:absolute before:inset-0 before:ml-5 before:-translate-x-px md:before:mx-auto md:before:translate-x-0 before:h-full before:w-0.5 before:bg-gradient-to-b before:from-transparent before:via-slate-300 before:to-transparent">
                   {this.createTimeLine()}
                 </div>
